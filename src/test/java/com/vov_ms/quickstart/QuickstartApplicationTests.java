cddbed94643e32238a09b8663466d8456b697b77
Volodymyr Kulesha <v.s.kulesha@gmail.com>
2023-12-10 14:39:50 +0200

    init proj

---
+++ src/test/java/com/vov_ms/quickstart/QuickstartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vov_ms.quickstart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuickstartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
